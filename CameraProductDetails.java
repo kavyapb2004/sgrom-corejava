e4e108a4c19dfb72109564e11eec3cef783223ab
kavyapb2004 <kavyapb2003@gmail.com>
2026-02-06 12:35:29 +0530

    java add variable task

---
+++ CameraProductDetails.java
@@ -0,0 +1,28 @@
+class CameraProductDetails {
+    public static void main(String[] args) {
+
+        String productName = "DSLR Camera";
+        String brand = "Canon";
+        int price = 52000;
+        String category = "Electronics";
+        String seller = "Amazon";
+        String resolution = "24.1 MP";
+        String lensType = "18-55mm";
+        String videoRecording = "4K";
+        int warrantyYears = 2;
+        String color = "Black";
+        float rating = 4.5f;
+
+        System.out.println("PRODUCT NAME: " + productName);
+        System.out.println("BRAND: " + brand);
+        System.out.println("PRICE: " + price);
+        System.out.println("CATEGORY: " + category);
+        System.out.println("SELLER: " + seller);
+        System.out.println("RESOLUTION: " + resolution);
+        System.out.println("LENS TYPE: " + lensType);
+        System.out.println("VIDEO RECORDING: " + videoRecording);
+        System.out.println("WARRANTY: " + warrantyYears);
+        System.out.println("COLOR: " + color);
+        System.out.println("RATING: " + rating);
+    }
+}
---
+++ CookingClassRegistration.java
@@ -0,0 +1,13 @@
+class CookingClassRegistration {
+    public static void main(String[] cookingclassregistration) {
+
+        String studentName = "Anu";
+        String courseName = "Baking Basics";
+        String platform = "SkillShare";
+
+        System.out.println("Online Cooking Class Registration");
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Platform: " + platform);
+    }
+}
